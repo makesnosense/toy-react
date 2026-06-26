fd4d7b857119dd6801f17c425c00a4dd3c80a1f0
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2026-06-26 22:07:14 +0300

    after STEP 0: basic app without React

--- main.tsx
+++ main.tsx
@@ -1,10 +1,42 @@
-import * as Didact from "./didact";
+// import * as Didact from "./didact";
 
-const element = <h1 title="foo">Hello world1</h1>;
+// const element = <h1 title="foo">Hello world1</h1>;
 
-console.log("Element produced by JSX:", element);
+const element = {
+  type: "h1",
+  props: {
+    title: "foo",
+    children: ["Hello world1"],
+  },
+};
 
-// const root = document.getElementById("root");
+// console.log("Element produced by JSX:", element);
+
+const root = document.getElementById("root");
+
+const node = document.createElement(element.type);
+node["title"] = element.props.title;
+
+// NOTE!
+// why not node.innerText:
+// cause we want to have reference for a created child
+// and node.innerText just sets the string.
+// it's a property mutation, not a node-producing operation
+
+// why not node.innerHTML = "Hello":
+// innerHTML produces new nodes, but we do not get refences to them
+// we'd have to go fish for it via node.firstChild or a querySelector,
+// which is exactly the kind of re-discovery the fiber model is designed to avoid
+
+// and also XSS textbook case
+
+const text = document.createTextNode("");
+text["nodeValue"] = element.props.children[0];
+
+node.appendChild(text);
+
+root.appendChild(node);
+
 // if (root) {
 //   // root.textContent = "Hello world";
 // }
